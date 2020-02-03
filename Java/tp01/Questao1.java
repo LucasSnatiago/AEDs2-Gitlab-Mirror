12031d5104d3544a2148c5bff2032461458de8d0
Lucas Santiago <lu.santi.oli@gmail.com>
2020-02-03 08:45:45 -0300

    Mais uma

---
+++ Java/tp01/Questao1.java
@@ -0,0 +1,40 @@
+class Questao1{
+    public static void main(String[] args){        
+        String entrada = MyIO.readLine();
+
+        while(!ehFim(entrada)){
+
+            if(ehPalindromo(entrada)){
+                System.out.println("SIM");
+            }else{
+                System.out.println("NAO");
+            }
+
+            entrada = MyIO.readLine();
+        }
+
+    }
+
+    public static boolean ehFim(String entrada) {  //Descobrir se eh o fim do arquivo
+        boolean fim = true;
+        if(entrada.charAt(0) != 'F' || entrada.charAt(1) != 'I' || entrada.charAt(2) != 'M'){
+            fim = false;
+        }
+        return fim;
+    } 
+
+
+    public static boolean ehPalindromo(String entrada){
+        boolean ehPalindromo = true;
+        int posFinal = entrada.length();
+
+        for(int i = 0; i < entrada.length(); i++){
+            if(entrada.charAt(i) != entrada.charAt(posFinal-1)){
+                ehPalindromo = false;
+            }
+        }
+
+        return ehPalindromo;
+    }
+
+}
