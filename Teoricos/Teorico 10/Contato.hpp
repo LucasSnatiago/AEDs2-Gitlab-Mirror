c1c3b76d54f83897db2d6ae533c9c45d9f0c3abd
Lucas Santiago <lu.santi.oli@gmail.com>
2020-05-14 20:15:35 -0300

    Contato estilizado!

--- Teoricos/Teorico 10/Contato.hpp
+++ Teoricos/Teorico 10/Contato.hpp
@@ -15,18 +15,18 @@ public:
 
     //Criando um Contato
     Contato() {
-        this->nome = "";
-        this->email = "";
-        this->telefone = 0;
-        this->cpf = 0;
+        this->nome      = "";
+        this->email     = "";
+        this->telefone  = 0;
+        this->cpf       = 0;
     }
 
     //Inserir um novo contato
     void inserir(string nome, string email, int telefone, int cpf) {
-        this->nome = nome;
-        this->email = email;
-        this->telefone = telefone;
-        this->cpf = cpf;
+        this->nome       = nome;
+        this->email      = email;
+        this->telefone   = telefone;
+        this->cpf        = cpf;
     }
 
     void mostrar() {
